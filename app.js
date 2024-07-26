21cc73b70e00edcd1b95d4de76e9549dba707327
Sam Kim <samuelkim9241@gmail.com>
2024-07-25 23:43:42 -0400

    database intialization

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //LOAD ENV VARIABLES
 require("dotenv").config();
 
-
 //express app
 const express = require('express');
 const app = express();
-
+const connect = require("./database/mongodb");
+connect();
 //routing
 app.get('/',(req,res) => {
     res.json({hello : "world"});
