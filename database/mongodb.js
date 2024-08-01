a21cf3aa0c6a0f613a75a1e9145cd304a5dde3e4
Sam Kim <samuelkim9241@gmail.com>
2024-08-01 05:51:05 -0400

    mongoose

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,4 +1,11 @@
-function connectMongoDb() {
-    console.log("hello");
+if (process.env.NODE_ENV != "production") {
+    require ("dotenv").config();
+}
+
+
+
+const mongoose = require("mongoose");
+async function connectMongoDb() {
+    await mongoose.connect(process.env.Database);
 }
 module.exports=connectMongoDb;
