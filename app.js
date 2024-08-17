e374914c5ee2e1ab27f5f14424c0474053a8ba7c
Sam Kim <samuelkim9241@gmail.com>
2024-08-17 19:21:47 -0400

    notes

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ require("dotenv").config();
 
 //express app
 const express = require('express');
-//connect to DN
+//connect to DB
 
 
 const connectToDb = require("./config/connectToDb");
+const Note = require("./designs/notes")
 const app = express();
 app.use(express.json());
 connectToDb();
@@ -15,8 +16,14 @@ connectToDb();
 app.get('/',(req,res) => {
     res.json({hello : "world"})
 });
-app.post("/notes",(req,res)+>{
+app.post("/notes",async (req,res)=>{
+    const title = req.body.title
+    const body = req.body.body
 
+    const note = await Note.create({
+        title: title,
+        body: body,
+})
 });
 //server
 app.listen(process.env.PORT);
