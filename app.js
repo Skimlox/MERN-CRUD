ba300a2693aba780e279ef9d59e366dc094f7c63
Sam Kim <samuelkim9241@gmail.com>
2024-08-31 23:59:30 -0400

    updates

--- app.js
+++ app.js
@@ -2,60 +2,27 @@
 //LOAD ENV VARIABLES
 require("dotenv").config();
 
-//express app
+//dependencies
 const express = require('express');
-//connect to DB
+const cors = require("cors");
+const connectToDb = require("./main-server/config/connectToDb");
+const Note = require("./designs/notes");
+const notesController = require("./controllers/notescontrol");
 
-
-const connectToDb = require("./config/connectToDb");
-const Note = require("./designs/notes")
+//app config
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectToDb();
-//routing
+
 
 
 //requests
-app.get('/',(req,res) => {
-    res.json({hello : "world"})
-});
-app.get('/notes',async (req,res) => {
-    const notes = await Note.find();
-    res.json({notes : notes});
-});
-app.get('/notes/:id',async (req,res) => {
-    const noteID = req.params.id;
-    const note = await Note.findById(noteID);
-    res.json({notes : notes});
-});
-
-app.post("/notes",async (req,res)=>{
-    const title = req.body.title;
-    const body = req.body.body;
-
-    const note = await Note.create({
-        title: title,
-        body: body,
-})
-});
-app.put('/notes/:id',async (req,res)=>{
-    const noteId = req.params.id;
-    const title = req.body.title;
-    const body = req.body.body;
-    await Note.findByIdAndUpdate(noteId, {
-        title: title,
-        body: body,
-    });
-    //updated note
-    const note = await Note.findById(noteId);
-    //response 
-    res.json({notes : notes});
-});
-app.delete('/notes/:id',async (req,res)=>{
-    const noteId = req.params.id;
-    await Note.deleteOne({id: noteId});
-    res.json({success: "Record deleted!"});
-});
+app.get("/notes", notesController.fetchNotes);
+app.get("/notes/:id", notesController.fetchNote);
+app.post("/notes", notesController.createNote);
+app.put("/notes/:id", notesController.updateNote);
+app.delete("/notes/:id", notesController.deleteNote);
 
 //server start
 app.listen(process.env.PORT);
--- main-server/config/connectToDb.js
+++ main-server/config/connectToDb.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV != "production") {
 async function connectToDb() {
     try {
         await mongoose.connect(process.env.DB_URL);
-        console.log("Connected to DBA")
+        console.log("Connected to DB")
 } catch (error){
     console.log(error)
 }
